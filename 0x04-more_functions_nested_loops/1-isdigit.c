fc1166bbc3af0235a239e3fcffee7f20c85d290f
Tboy5952 <Morakinyobabatope@gmail.com>
2023-08-19 08:08:51 -0700

    putchar

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two number
+ * @a:parameter 1
+ * @b:parameter 2
+ * Return: 0 or 1
+ */
+int _isdigit(int a, int b)
+{
+	return (a * b);
+}
