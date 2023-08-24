da032cd488ebe6887e510b8b54d1931e2b131d4a
Tboy5952 <Morakinyobabatope@gmail.com>
2023-08-24 09:45:27 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverses the content 
+ * @a: parameter 1
+ * @n: parameter 2
+ * Return: Always 0
+ */
+void reverse_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' i++)
+
