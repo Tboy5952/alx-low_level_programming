0e33515696620029e3918d9e8dc53699d18e2e7c
Tboy5952 <Morakinyobabatope@gmail.com>
2023-08-25 01:07:37 -0700

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,13 @@
 char *string_toupper(char *n)
 {
 	int i;
-	
+
 	i = 0;
 	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-		{
 			n[i] = n[i] - 32;
-			i++;
-		}
+		i++;
 	}
 	return (n);
 }
