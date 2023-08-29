d919669beb1786426570cda1e78629248662e325
Tboy5952 <Morakinyobabatope@gmail.com>
2023-08-29 08:02:59 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0 ; s[i] != '\0'; i++)
 	{
-		for (k = 0 ; s[i] != accept[k] ;k++)
+		for (k = 0 ; s[i] != accept[k] ; k++)
 		{
 			if (accept[k] == '\0')
 				return (i);
