ac56f9d334945d6266405615cca2497faf26408e
Tboy5952 <Morakinyobabatope@gmail.com>
2023-09-03 05:30:02 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main-programme that string 
- * @agrc:parameter that count the number of strings
- * @agrv:pointer that point to the string 
- * Return: Always 0 success
- */
+ *main-Programme that its file name
+* @agrc:parameter that count the number of strings
+*@agrv:argument vector
+* Return: Always 0 success
+*/
 int main(int agrc, char **agrv)
 {
 	(void) agrc; /* declear unused argument */
-	printf("%s\n",agrv[0]); /* agrv[0] will print file name */
-	return(0);
+	printf("%s\n", agrv[0]) ; /* agrv will print name */
+	return (0);
 }
