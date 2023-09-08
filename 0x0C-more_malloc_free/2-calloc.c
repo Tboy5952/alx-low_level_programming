7f6bade8ad570066986db5f1caee09d8c7d21030
Tboy5952 <Morakinyobabatope@gmail.com>
2023-09-08 04:27:59 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_calloc- ffunction that allocate memory for an array
+ *@nmemb:number of array
+ *@size:size of array
+ *
+ * Return:pointer to a newly allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *output; /* declearation of a pointer */
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	output = calloc(nmemb, size);  /* allocate memory */
+	if (output == NULL)
+		return (NULL);
+	else
+
+		return (output);
+}
