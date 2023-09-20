68478ed2eaad60fd27967b635af3c145ea1f122d
Tboy5952 <morakinyobabatope@gmail.com>
2023-09-20 13:29:52 -0700

    1-isdigit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,8 +4,7 @@ int _isupper(int c);
 int _isdigit(int a, int b);
 int mul(int a, int b);
 void print_numbers(void);
-#include <stdio.h>
-#include <unistd.h>
+int _putchar(char)
 void print_most_numbers(void);
 void more_numbers(void);
 #endif
