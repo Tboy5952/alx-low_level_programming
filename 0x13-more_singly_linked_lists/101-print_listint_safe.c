6d6fa9875f66ccca095b0aa99268c79b2d0d06bc
Tboy5952 <morakinyobabatope@gmail.com>
2023-09-27 00:36:48 -0700

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * print_listint_safe - function that prints a listint_t
+  * @head:node head
+  * Return:Always 0 sucess
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
